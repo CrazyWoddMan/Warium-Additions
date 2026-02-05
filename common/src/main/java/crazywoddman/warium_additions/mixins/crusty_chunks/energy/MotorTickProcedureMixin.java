fef245ef8818fec54cba63d79a65bd19d1ff3323
CrazyWoddMan <mishaocheret@gmail.com>
2026-02-05 02:24:13 +0100

    1.0.4: Electric Motor doen't make sound when working fix

--- common/src/main/java/crazywoddman/warium_additions/mixins/crusty_chunks/energy/MotorTickProcedureMixin.java
+++ common/src/main/java/crazywoddman/warium_additions/mixins/crusty_chunks/energy/MotorTickProcedureMixin.java
@@ -5,12 +5,17 @@ import net.mcreator.crustychunks.procedures.MotorTickProcedure;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.level.Level;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Optional;
 
@@ -31,7 +36,7 @@ public class MotorTickProcedureMixin {
         remap = false
     )
     private static void rewriteExecute(LevelAccessor world, double x, double y, double z, CallbackInfo ci) {
-        if (world instanceof Level level) {
+        if (world instanceof ServerLevel level) {
             BlockPos pos = BlockPos.containing(x, y, z);
             BlockEntity blockEntity = level.getBlockEntity(pos);
             int kineticPower = (int) Math.round(blockEntity.getCapability(ForgeCapabilities.ENERGY).map(storage -> {
@@ -41,35 +46,38 @@ public class MotorTickProcedureMixin {
                     Direction facing = blockEntity.getBlockState().getValue(LargeElectricMotorBlock.FACING);
                     energy = Optional.ofNullable(level.getBlockEntity(pos.relative(facing.getOpposite()))).map(connected -> 
                         storage.receiveEnergy(
-                            connected.getCapability(ForgeCapabilities.ENERGY, facing).map(cap -> {
-                                int extracted =
+                            connected.getCapability(ForgeCapabilities.ENERGY, facing).map(cap ->
                                 cap.canExtract()
-                                ? cap.extractEnergy(storage.receiveEnergy(Math.min(Config.SERVER.energyTransferLimit.get(), Config.SERVER.motorConsumptionLimit.get()), true), false)
-                                : 0;
-                                // if (extracted != 0)
-                                    System.out.println("extracted: " + extracted);
-                                return extracted;
-                            }).orElse(0),
+                                    ? cap.extractEnergy(storage.receiveEnergy(Math.min(Config.SERVER.energyTransferLimit.get(), Config.SERVER.motorConsumptionLimit.get()), true), false)
+                                    : 0
+                            ).orElse(0),
                             false
                         )
                     ).orElse(0);
                 }
+
+                if (energy != 0)
+                    level.playSound(
+                        null,
+                        BlockPos.containing(x, y, z),
+                        ForgeRegistries.SOUND_EVENTS.getValue(ResourceLocation.fromNamespaceAndPath("crusty_chunks", "motor")),
+                        SoundSource.BLOCKS,
+                        0.2F,
+                        Mth.nextFloat(RandomSource.create(), 1.2f, 1.3f)
+                    );
                 
                 return energy;
             }).orElse(0) / Config.SERVER.kineticToFeRate.get());
 
             CompoundTag data = blockEntity.getPersistentData();
-            BlockState state;
+            BlockState state = blockEntity.getBlockState();
 
             if (data.getDouble("KineticPower") != kineticPower) {
                 data.putInt("KineticPower", kineticPower);
-                state = blockEntity.getBlockState();
                 level.sendBlockUpdated(pos, state, state, Block.UPDATE_ALL);
             }
-            // else if (level.getGameTime() % 20 == 0) {
-                state = blockEntity.getBlockState();
+            else
                 level.sendBlockUpdated(pos, state, state, Block.UPDATE_NONE);
-            // }
         }
     
         ci.cancel();
